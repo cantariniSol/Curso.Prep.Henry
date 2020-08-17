d3414431d69dc19163b2b045794bd0a4cde0c0ed
María Sol Cantarini <54611951+cantariniSol@users.noreply.github.com>
2020-08-17 01:30:24 -0300

    Homework done!

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,138 +1,157 @@
 // No cambies los nombres de las funciones.
 
-function obtenerMayor(x, y) {
-  // "x" e "y" son números enteros (int).
-  // Devuelve el número más grande
-  // Si son iguales, devuelve cualquiera de los dos
+function devolverPrimerElemento(array) {
+  // Devuelve el primer elemento de un array
   // Tu código:
-  var mayor = 0;
-  if( x > y){
-    mayor = x;
-  }
-  else {
-    mayor = y;
-  }
-  return mayor;
+  
+  return array[0];
 }
 
-function saludo(idioma) {
-  // Devuelve un saludo en tres diferentes lenguajes:
-  // Si "idioma" es "aleman", devuelve "Guten Tag!"
-  // Si "idioma" es "mandarin", devuelve "Ni Hao!"
-  // Si "idioma" es "ingles", devuelve "Hello!"
-  // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
+function devolverUltimoElemento(array) {
+  // Devuelve el último elemento de un array
   // Tu código:
-   var saludoAleman = 'Guten Tag!';
-   var saludoMandarin = 'Ni Hao!';
-   var saludoIngles = 'Hello!';
-   var saludoUd = 'Hola!';
-
-   if(idioma === 'aleman'){
-     return saludoAleman;
-   }
-   else if(idioma === 'mandarin'){
-     return saludoMandarin;
-   }
-   else if(idioma === 'ingles'){
-     return saludoIngles;
-   }
-   else {
-     return saludoUd;
-   }
+   return array[array.length - 1]; //toma el largo de la lista y le resta una asi da cómo ultima posición del arreglo.
 }
 
-function esDiezOCinco(numero) {
-    // Devuelve "true" si "numero" es 10 o 5
-    // De lo contrario, devuelve "false"
-    // Tu código:
-    if(numero === 5 || numero === 10){
+function obtenerLargoDelArray(array) {
+  // Devuelve el largo de un array
+  // Tu código:
+
+  return array.length;
+}
+
+function incrementarPorUno(array) {
+  // "array" debe ser una matriz de enteros (int/integers)
+  // Aumenta cada entero por 1
+  // y devuelve el array
+  // Tu código:
+  var sumandoUno = [];
+   for (var n = 0; n < array.length; n ++){
+      sumandoUno[n] = array[n] + 1;
+   }
+
+   return sumandoUno;
+}
+
+function agregarItemAlFinalDelArray(array, elemento) {
+  // Añade el "elemento" al final del array
+  // y devuelve el array
+  // Tu código:
+   array[array.length] = elemento;
+   return array;
+}
+
+function agregarItemAlComienzoDelArray(array, elemento) {
+  // Añade el "elemento" al comienzo del array
+  // y devuelve el array
+  // Pista: usa el método `.unshift`
+  // Tu código:
+  array.unshift(elemento);
+  return array;
+}
+
+function dePalabrasAFrase(palabras) {
+  // "palabras" es un array de strings/cadenas
+  // Devuelve un string donde todas las palabras estén concatenadas
+  // con espacios entre cada palabra
+  // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
+  // Tu código:
+  var cadena = '';
+  cadena = palabras.join( ' ');
+  return cadena;
+}
+
+function arrayContiene(array, elemento) {
+  // Comprueba si el elemento existe dentro de "array"
+  // Devuelve "true" si está, o "false" si no está
+  // Tu código:
+  
+  for(var n = 0; n < array.length; n++){
+    if (array[n] === elemento){
       return true;
-    }
-    else {
-      return false;
-    }
-}
-
-function estaEnRango(numero) {
-  // Devuelve "true" si "numero" es menor que 50 y mayor que 20
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  rango = false;
-  if(numero > 20 && numero < 50 ){
-  rango = true;
-  return rango;
+    } 
   }
-  return rango;
-}
-
-
-function esEntero(numero) {
-  // Devuelve "true" si "numero" es un entero (int/integer)
-  // Ejemplo: 0.8 -> false
-  // Ejemplo: 1 -> true
-  // Ejemplo: -10 -> true
-  // De lo contrario, devuelve "false"
-  // Pista: Puedes resolver esto usando `Math.floor`
-  // Tu código:
-       if (numero === Math.floor(numero)){
-         return true;
-       }
-       else{
-         return false;
-       }
-}
-
-
-function fizzBuzz(numero) {
-  // Si "numero" es divisible entre 3, devuelve "fizz"
-  // Si "numero" es divisible entre 5, devuelve "buzz"
-  // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
-  // De lo contrario, devuelve el numero
-    if( numero % 3 === 0){
-      return 'fizz';
-    }
-     
-    else if( numero % 5 === 0){
-      return 'buzz';
-    }
-      
-    else if((numero % 3 === 0) && (numero % 5 === 0)) { 
-      return 'fizzbuzz';
-    }
-    
-    
-  return numero;
-}
-
-function esPrimo(numero) {
-  // Devuelve "true" si "numero" es primo
-  // De lo contrario devuelve "falso"
-  // Pista: un número primo solo es divisible por sí mismo y por 1
-  // Pista 2: Puedes resolverlo usando un bucle `for`
-  // Nota: Los números 0 y 1 NO son considerados números primos
-  if (numero <= 1)
   return false;
-  if (numero === 2)
-  return true;
-  for (n = 3; n < numero; n++){
-    if (numero % n === 0){
-      return false;
-    }
-  }
-  return true;
 }
 
+function agregarNumeros(numeros) {
+  // "array" debe ser una matriz de enteros (int/integers)
+  // Suma todos los enteros y devuelve el valor
+  // Tu código:
+  var sumar = 0;
+  for(var n = 0; n < numeros.length; n++) {
+    sumar += numeros[n];
+  }
+  return sumar;
 
+}
+
+function promedioResultadosTest(resultadosTest) {
+  // "resultadosTest" debe ser una matriz de enteros (int/integers)
+  // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
+  // Tu código:
+  var promedio = 0;
+  var suma = 0;
+  for(var n = 0; n < resultadosTest.length; n++){
+    sumaNumeros = agregarNumeros(resultadosTest);
+  }
+   Math.round(promedio = sumaNumeros / resultadosTest.length);
+  return promedio;
+}
+
+function numeroMasGrande(numeros) {
+  // "numeros" debe ser una matriz de enteros (int/integers)
+  // Devuelve el número más grande
+  // Tu código:
+    var grande = 0;
+    for(var n = 0; n < numeros.length; n++){
+      if (numeros[n] > grande){
+        grande = numeros[n];
+      }
+    }
+    return grande;
+}
+
+function multiplicarArgumentos() {
+  // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
+  // Si no se pasan argumentos devuelve 0
+  // Si se pasa un argumento, simplemente devuélvelo
+  // Tu código:
+  //Las funciones avaScript tienen un objeto incorporado llamado objeto de argumentos.
+
+//argument contiene una matriz de los argumentos utilizados cuando se llama (invoca) la función.
+
+var produc = 1;
+var unoSolito = 0;
+  if (arguments.length === 0 ){
+    return 0;
+  }
+  else if(arguments.length === 1){
+   unoSolito = arguments[0];
+   return unoSolito;
+  }
+  else{
+    for(var n =0; n < arguments.length; n++){
+       produc *= arguments[n];
+    }
+  }
+  return produc;
+}
 
 // No modificar nada debajo de esta línea
 // --------------------------------
 
 module.exports = {
-  obtenerMayor,
-  saludo,
-  esDiezOCinco,
-  estaEnRango,
-  esEntero,
-  fizzBuzz,
-  esPrimo
+  devolverPrimerElemento,
+  devolverUltimoElemento,
+  obtenerLargoDelArray,
+  incrementarPorUno,
+  agregarItemAlFinalDelArray,
+  agregarItemAlComienzoDelArray,
+  dePalabrasAFrase,
+  arrayContiene,
+  agregarNumeros,
+  promedioResultadosTest,
+  numeroMasGrande,
+  multiplicarArgumentos,
 };
